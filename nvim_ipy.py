a55e77f55a5aec37a1dda2e5b34122baaf034d91
Björn Linse <bjorn.linse@gmail.com>
2014-07-12 23:43:39 +0200

    integrate with ipython console's inputhook

--- nvim_ipy.py
+++ nvim_ipy.py
@@ -3,6 +3,8 @@ import neovim
 class IPythonPlugin(object):
     def __init__(self, vim):
         self.vim = vim
+        self.create_outbuf()
+        self.vim.subscribe("ipy_runline")
 
     def create_outbuf(self):
         vim = self.vim
@@ -44,19 +46,32 @@ class IPythonPlugin(object):
         line = self.get_selection('line')
         self.append_outbuf(line)
 
-    def do_ev(self):
-        msg = self.vim.next_message()
+    def do_ev(self,timeout=None):
+        try:
+            msg = self.vim.next_message(timeout)
+        except NameError: #FIXME: upstream should emit TimeoutError
+            return 0
         method = "on_" + msg.name
         if hasattr(self, method):
             getattr(self, method)(msg)
         else:
             print("warning: ignore", msg.name)
+        return 0 #inputhook shall return 0
 
     def run(self):
-        self.create_outbuf()
-        self.vim.subscribe("ipy_runline")
         while True: self.do_ev()
+
+    # debug ipython client plugint using ipython console...
+    def ipython_inputhook_register(self):
+        from IPython.lib.inputhook import inputhook_manager
+        inputhook_manager.set_inputhook(lambda: self.do_ev(0))
         
+def test_ipython(path):
+    vim = neovim.connect(path)
+    p = IPythonPlugin(vim)
+    p.ipython_inputhook_register()
+    return vim, p
+
 if __name__ == "__main__":
     vim = neovim.connect(sys.argv[1])
     IPythonPlugin(vim).run()
